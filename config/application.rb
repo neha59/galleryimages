aa929a653bb834f49d215ba2cd5522f51da4ad7f
neha59 <nehaneharani0@gmail.com>
2020-04-29 11:50:16 +0530

    commit

--- config/application.rb
+++ config/application.rb
@@ -21,32 +21,8 @@ module Gallery
       :address          => 'smtp.gmail.com',
       :domain           => 'mail.google.com',
       :port             =>  587,
-      :user_name        => "rahulsingh.bluebash@gmail.com",
-      :password         => "r@hulSingh8786",
-      :authentication   => 'login',
-      :enable_starttls_auto  => true
-    }
-
-
-  end
-end
-
-module Deviseapp
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    ActionMailer::Base.smtp_settings = {
-
-      :address          => 'smtp.gmail.com',
-      :domain           => 'mail.google.com',
-      :port             =>  587,
-      :user_name        => "rahulsingh.bluebash@gmail.com",
-      :password         => "r@hulSingh8786",
+      :user_name        => "neha.bluebash@gmail.com",
+      :password         => "neharani1998",
       :authentication   => 'login',
       :enable_starttls_auto  => true
     }
